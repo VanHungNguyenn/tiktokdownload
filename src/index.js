5d6b66e9977a05b3730e15c208996e8c721a662c
VanHungNguyen <vanhungnguyenbku@gmail.com>
2022-03-25 11:35:51 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './css/reset.css'
-import 'antd/dist/antd.css'
 import './css/index.css'
+
 import App from './App'
 
 ReactDOM.render(
